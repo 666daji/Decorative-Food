80018b38ecd42da8dae0210fbec66fc6f46b6d73
666daji <3391852095@qq.com>
2025-07-21 19:14:11 +0800

    为种子添加了配方

--- src/main/java/org/dfood/ThreedfoodDataGenerator.java
+++ src/main/java/org/dfood/ThreedfoodDataGenerator.java
@@ -2,12 +2,17 @@ package org.dfood;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import org.jetbrains.annotations.Nullable;
 
 public class ThreedfoodDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+	}
 
+	@Override
+	public @Nullable String getEffectiveModId() {
+		return "dfood";
 	}
 }
